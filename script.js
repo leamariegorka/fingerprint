42fa9f06085751c73f74fa1a55f218e2d0b47917
Lea-Marie Gorka <147590906+leamariegorka@users.noreply.github.com>
2025-11-23 13:19:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function gruss() {
+  const startTime = performance.now();
 
+  console.log("start");
+
+  let array;
+  array = new Uint32Array(100);
+  
+  for (let j = 0; j < 1000; j++) {
+  
+    crypto.getRandomValues(array);
+
+    let sum = 0;
+    for (let i = 0; i < 100; i++) {
+      sum += array[i];
+    }
+    
+  }
+
+  const endTime = performance.now();
+  alert("Zeit: " + (endTime - startTime).toFixed(3));
+
+} 
