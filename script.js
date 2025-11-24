f4c37b1a7213e21e59e28209ca8cbbd9ad288a8b
Lea-Marie Gorka <147590906+leamariegorka@users.noreply.github.com>
2025-11-24 11:01:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 function gruss() {
   const startTime = performance.now();
 
-  console.log("start");
-
   if(self.crossOriginIsolated){
     console.log("ja");
   }
 
   let array;
-  array = new Uint32Array(100);
+  array = new Uint32Array(1000);
   
-  for (let j = 0; j < 1000; j++) {
+  //for (let j = 0; j < 1000; j++) {
   
     crypto.getRandomValues(array);
 
-    let sum = 0;
-    for (let i = 0; i < 100; i++) {
-      sum += array[i];
-    }
+    //let sum = 0;
+    //for (let i = 0; i < 100; i++) {
+      //sum += array[i];
+    //}
     
-  }
+  //}
 
   const endTime = performance.now();
   alert("Zeit: " + (endTime - startTime).toFixed(3));
