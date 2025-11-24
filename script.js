f50af801af6bdd927da6141c4c6f47ff164686d5
Lea-Marie Gorka <147590906+leamariegorka@users.noreply.github.com>
2025-11-24 14:10:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-function gruss() {
-  const startTime = performance.now();
+function FP() {
+  //const startTime = performance.now();
 
-  if(self.crossOriginIsolated){
-    console.log("ja");
+  const m = 3;
+  const n = 5;
+
+  //if(self.crossOriginIsolated){
+    //console.log("ja");
+  //}
+  let arrays = [];
+  let values = [];
+  for (let i = 0; i < m; i++){
+    values = [];
+    for (let j= 0; j < n; j++){
+      const startTime = performance.now();
+      let array;
+      array = new Uint32Array(1000);
+      crypto.getRandomValues(array);
+      const endTime = performance.now();
+      const logTime = endTime - startTime;
+      values.push(logTime);
+    }
+    arrays.push(values);
   }
 
-  let array;
-  array = new Uint32Array(1000);
-  
-  //for (let j = 0; j < 1000; j++) {
-  
-    crypto.getRandomValues(array);
+  console.log(arrays);
+
+  alert("geschafft");
+      
 
     //let sum = 0;
     //for (let i = 0; i < 100; i++) {
@@ -19,7 +35,9 @@ function gruss() {
     
   //}
 
-  const endTime = performance.now();
-  alert("Zeit: " + (endTime - startTime).toFixed(3));
+  //const endTime = performance.now();
+  //alert("Zeit: " + (endTime - startTime).toFixed(3));
 
 } 
+
+
