041c2be907d0ed8dc15e4a4e2c6f9446dfafd0a4
Lea-Marie Gorka <147590906+leamariegorka@users.noreply.github.com>
2025-11-25 14:15:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ function FP() {
   for (let i = 0; i < m; i++){
     values = [];
     for (let j= 0; j < n; j++){
-      const startTime = performance.now();
+      const startTime = date.now();
       let array;
       array = new Uint32Array(1000);
       crypto.getRandomValues(array);
-      const endTime = performance.now();
+      const endTime = date.now();
       const logTime = endTime - startTime;
       values.push(logTime);
     }
